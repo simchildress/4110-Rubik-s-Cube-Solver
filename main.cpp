b1a667eaf14e76a493ed52c1069ac94e4285d25a
Simeon <121066986+simchildress@users.noreply.github.com>
2025-07-17 16:27:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -13,13 +13,13 @@ Cube myCube;
 //makes cube from user input
     myCube.input();
     
-
+/*
 char turn = "R";
 myCube.move();
 
 cout << "Turning the right face of your cube (orange face)\n";
 
-
+*/
 
 
 
