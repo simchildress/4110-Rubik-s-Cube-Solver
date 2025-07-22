c51b48443fee773e14bff429fcfec22a3ab936aa
Simeon <121066986+simchildress@users.noreply.github.com>
2025-07-22 10:35:54 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -12,10 +12,12 @@ Cube myCube;
 
 //makes cube from user input
     myCube.input();
-    
+
+    cout << "Here is your original cube:\n\n";  
 //prints the cube they inputed  
     myCube.print();
 
+    cout << "Here is your new cube:\n\n";
 //Turns Cube then prints (test)
     myCube.move("D");
     myCube.print();
